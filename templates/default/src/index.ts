9b946a44fc79ab6684fa1ab36ceb83d84147331a
Herberth <herberthobregon@gmail.com>
2021-06-08 09:57:13 -0600

    update dependencies

--- templates/default/src/index.ts
+++ templates/default/src/index.ts
@@ -3,17 +3,17 @@ import { customElement, property } from "lit/decorators.js";
 
 @customElement("{{name}}")
 export class {{capital name}} extends LitElement {
-	static styles = [
-		css`
-			:host {
-				display: block;
-			}
-		`
-	];
+    static styles = [
+        css`
+            :host {
+                display: block;
+            }
+        `
+    ];
 
-	@property() name = "{{capital name space=true}}";
+    @property() name = "{{capital name space=true}}";
 
-	render() {
-		return html`<h1>Hello, ${this.name}</h1>`;
-	}
+    render() {
+        return html`<h1>Hello, ${this.name}</h1>`;
+    }
 }
