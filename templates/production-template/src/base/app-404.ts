c22cd41865baba61717102d7e3bc97e866a35a16
Herberth <herberthobregon@gmail.com>
2022-01-28 00:38:53 -0600

    fixed #8, updated packages

--- templates/production-template/src/base/app-404.ts
+++ templates/production-template/src/base/app-404.ts
@@ -1,15 +1,14 @@
-import { CtLit, html, property, customElement, css } from '@conectate/ct-lit/ct-lit';
+import { CtLit, html, property, customElement, css } from '@conectate/ct-lit';
 
 @customElement('app-404')
 export class App404 extends CtLit {
+	static styles = css`
+		:host {
+			display: block;
+		}
+	`;
 
-    static styles = css`
-    :host {
-        display: block;
-    }
-    `;
-
-    render() {
-        return html`<h1>404 - Not found</h1>`;
-    }
+	render() {
+		return html`<h1>404 - Not found</h1>`;
+	}
 }
--- templates/production-template/src/login/activities/app-login.ts
+++ templates/production-template/src/login/activities/app-login.ts
@@ -1,16 +1,14 @@
-import { CtLit, html, property, customElement, css } from '@conectate/ct-lit/ct-lit';
-
+import { CtLit, html, property, customElement, css } from '@conectate/ct-lit';
 
 @customElement('app-login')
 export class AppLogin extends CtLit {
+	static styles = css`
+		:host {
+			display: block;
+		}
+	`;
 
-    static styles = css`
-    :host {
-        display: block;
-    }
-    `;
-
-    render() {
-        return html`<h1>Login</h1>`;
-    }
+	render() {
+		return html`<h1>Login</h1>`;
+	}
 }
